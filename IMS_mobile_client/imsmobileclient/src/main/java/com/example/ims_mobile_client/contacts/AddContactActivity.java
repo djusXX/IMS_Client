bb03bc50683fc803d39132d7a1f78e00bcc34126
djus <dawidjuszczak@gmail.com>
2021-07-17 06:14:46 +0200

    add addContactActivity

--- IMS_mobile_client/imsmobileclient/src/main/java/com/example/ims_mobile_client/contacts/AddContactActivity.java
+++ IMS_mobile_client/imsmobileclient/src/main/java/com/example/ims_mobile_client/contacts/AddContactActivity.java
@@ -1,15 +1,53 @@
 package com.example.ims_mobile_client.contacts;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.ims_mobile_client.R;
+
+import java.util.ArrayList;
+
 public class AddContactActivity extends AppCompatActivity {
+
+    ArrayList<Contact> contacts;    // TODO: change to db connection or something similar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add_contact);
+        EditText displayName = findViewById(R.id.add_contact_display_name);
+        EditText sipUri = findViewById(R.id.add_contact_sip_uri);
+        Button add = findViewById(R.id.add_contact_add);
+        Button cancel = findViewById(R.id.add_contact_cancel);
+
+
+
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String contactName = displayName.getText().toString();
+                String contactSipUri = sipUri.getText().toString();
+
+
+                if(!contactName.isEmpty() || !contactSipUri.isEmpty()) {
+                    contacts.add(new Contact(contactName, contactSipUri));
+                }
+                finish();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- IMS_mobile_client/imsmobileclient/src/main/java/com/example/ims_mobile_client/contacts/Contact.java
+++ IMS_mobile_client/imsmobileclient/src/main/java/com/example/ims_mobile_client/contacts/Contact.java
@@ -2,9 +2,15 @@ package com.example.ims_mobile_client.contacts;
 
 public class Contact {
     private String name = "contact_name";
+    String sipUri;
 
     public Contact(String name) {
         this.name = name;
+    }
+
+    public Contact(String name, String sipUri) {
+        this.name = name;
+        this.sipUri = sipUri;
     }
 
     public String getName() {
