d15e86505a03e00ce9316a536d7202cd5e8581d0
djus <dawidjuszczak@gmail.com>
2022-02-10 18:29:48 +0100

    add db entities

---
+++ IMS_mobile_client/imsmobileclient/src/main/java/com/example/ims_mobile_client/data/MessageEntity.java
@@ -0,0 +1,31 @@
+package com.example.ims_mobile_client.data;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "messages_table")
+public class MessageEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @NonNull
+    public String sip_uri_FROM;
+
+    @NonNull
+    public String sip_uri_TO;
+
+    @NonNull
+    public String timestamp;    // time of sending/receiving the message
+
+    @NonNull
+    public String content;
+
+    public MessageEntity(String sip_uri_FROM, String sip_uri_TO, String timestamp, String content) {
+        this.sip_uri_FROM = sip_uri_FROM;
+        this.sip_uri_TO = sip_uri_TO;
+        this.timestamp = timestamp;
+        this.content = content;
+    }
+}
