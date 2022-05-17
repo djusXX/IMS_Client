9f496d32f35f7fd2da4b6ed06344c89bd29ed161
djus <dawidjuszczak@gmail.com>
2022-05-17 23:41:41 +0200

    Wrap class SendInstantMessageParam

---
+++ IMS_mobile_client/pjsua2IMS/src/main/java/ims_mobile_client/pjsua2IMS/P2IMessage.java
@@ -0,0 +1,9 @@
+package ims_mobile_client.pjsua2IMS;
+
+import org.pjsip.pjsua2.SendInstantMessageParam;
+
+public class P2IMessage extends SendInstantMessageParam {
+    public P2IMessage() {
+        super();
+    }
+}
